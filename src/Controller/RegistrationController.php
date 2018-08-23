4cff485495b5bf8fb3c635545f47075856624eb0
Maxime <maximebarber@gmail.com>
2018-08-23 16:53:40 +0200

    Created RegistrationController

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/registration", name="registration")
+     */
+    public function index()
+    {
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+}
