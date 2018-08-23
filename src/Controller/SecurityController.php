671cd66f71169aaf42e5fe0c9a925527467cb1fa
Maxime <maximebarber@gmail.com>
2018-08-23 21:45:08 +0200

    Login method

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
+     * @Route("/login", name="login")
      */
-    public function index()
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        // * 1. Get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // * 2. Get last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        // * 3. Create form
+        $form = $this->get('form.factory')
+            ->createNameBuilder(null)
+            ->add('_username', null, ['label' => 'Email'])
+            ->add('_password', PasswordType::class, ['label' => 'Mot de passe'])
+            ->add('ok', SubmitType::class, ['label' => 'Ok', 'attr' => ['class' => 'btn-primary btn-block']])
+            ->getForm();
+
+        return $this->render('security/login.html.twig', [
+            'mainLogin' => true, 'title' => 'Connexion',
+            //
+            'form' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
     }
 }
