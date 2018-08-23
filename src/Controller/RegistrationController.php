b460ed672bbbdfdf2110654bc0d05f98f19cf3f7
Maxime <maximebarber@gmail.com>
2018-08-23 21:21:50 +0200

    Created SecurityController

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegistrationController extends AbstractController
 {
